404559fd8a24bd2dc60bb89781a25c73844711de
Natali Knyazeva <knyazeva14@mail.ru>
2022-06-25 19:17:34 +0700

    Create RoleServiceImp.java

---
+++ src/main/java/com/example/service/RoleServiceImp.java
@@ -0,0 +1,25 @@
+package com.example.service;
+
+import com.example.model.Role;
+import com.example.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RoleServiceImp implements RoleService {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public List<Role> findAll() {
+        return roleRepository.findAll();
+    }
+
+    @Override
+    public Role getRoleByName(String name) {
+        return roleRepository.getRoleByName(name);
+    }
+}
