0adf397802614831f9157e03dd88f3d00c8a3161
Natali Knyazeva <knyazeva14@mail.ru>
2022-06-25 19:17:53 +0700

    Create UserRepository.java

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.repository;
+
+import com.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    public User getUserById(@Param("id") long id);
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User getUserByUsername(@Param("username") String username);
+}
